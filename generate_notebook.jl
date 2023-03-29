cf74aa24ce3dfeebfadb5fc3dce36a983925c39e
Oliver Schulz <oschulz@mpp.mpg.de>
2023-03-29 03:02:26 +0200

    Add Jupyter notebook generator

---
+++ generate_notebook.jl
@@ -0,0 +1,5 @@
+# This script is licensed under the MIT License (MIT).
+
+import Literate
+
+Literate.notebook("hep_ml_from_scratch.jl", ".", execute = false, name = "hep_ml_from_scratch", credit = true)
