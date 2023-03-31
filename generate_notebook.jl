28d35766ad2ec9029e6815c4b4ca367f9c24ff8f
Oliver Schulz <oschulz@mpp.mpg.de>
2023-03-31 10:50:13 +0200

    Make sure generate_notebook.jl runswith correct env

--- generate_notebook.jl
+++ generate_notebook.jl
@@ -1,5 +1,10 @@
 # This script is licensed under the MIT License (MIT).
 
+using Pkg
+Pkg.activate(@__DIR__)
+#Need to run this only once:
+Pkg.instantiate()
+
 import Literate
 
 Literate.notebook("hep_ml_from_scratch.jl", ".", execute = false, name = "hep_ml_from_scratch", credit = true)
